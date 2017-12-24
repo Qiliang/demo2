c31e75cd825a675bb7b65de71c63a3bee6de66d0
xiaoql <xiaoql@yeah.net>
2017-12-24 22:17:37 +0800

    Initial commit

---
+++ src/main/java/io/daocloud/demo2/Sample2Controller.java
@@ -0,0 +1,27 @@
+package io.daocloud.demo2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+@EnableAutoConfiguration
+public class Sample2Controller {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "from demo:" + restTemplate.getForObject("http://demo/", String.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Sample2Controller.class, args);
+    }
+}
